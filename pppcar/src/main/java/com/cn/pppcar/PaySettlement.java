5b8627141fd010a2ff66c6719558f191e9c5be3e
nurmemet <nurmemet09>
2016-04-04 22:28:53 +0800

    first

---
+++ pppcar/src/main/java/com/cn/pppcar/PaySettlement.java
@@ -0,0 +1,16 @@
+package com.cn.pppcar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nurmemet on 2016/4/4.
+ */
+public class PaySettlement extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_pay_settlement);
+    }
+}
